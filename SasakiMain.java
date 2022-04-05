d51d9f292c8617154bad71f1932096d6741a3000
arukreid <sasaki3y98521j@gmail.com>
2022-04-05 11:13:29 +0900

    SasakiMain作成

---
+++ SasakiMain.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class SasakiMain{
+	public static void main(String[] args){
+		System.out.print("幅>>");
+		int width = new Scanner(System.in).nextInt();
+		System.out.print("高さ>>");
+		int height = new Scanner(System.in).nextInt();
+		char[] maps = {'森','草','岩','水'};
+		char[][] mapCreates = new char[height][width];
+		for(int i=0;i<mapCreates.length;i++){
+			for(int j=0;j<mapCreates[i].length;j++){
+				int random = new Random().nextInt(4);
+				mapCreates[i][j] = maps[random];
+				System.out.print(mapCreates[i][j]+" ");
+			}
+			System.out.println();
+		}
+	}
+}
