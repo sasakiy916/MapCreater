0c4ea6a6ac46b7725926da9c2b68ab858c1f7f47
arukreid <sasaki3y98521j@gmail.com>
2022-04-07 11:42:23 +0900

    SasakiMain

--- SasakiMain.java
+++ SasakiMain.java
@@ -5,12 +5,12 @@ public class SasakiMain{
 		int width = new Scanner(System.in).nextInt();
 		System.out.print("高さ>>");
 		int height = new Scanner(System.in).nextInt();
-		char[] maps = {'森','草','岩','水'};
+		final char[] MAPS = {'森','草','岩','水'};
 		char[][] mapCreates = new char[height][width];
 		for(int i=0;i<mapCreates.length;i++){
 			for(int j=0;j<mapCreates[i].length;j++){
-				int random = new Random().nextInt(maps.length);
-				mapCreates[i][j] = maps[random];
+				int random = new Random().nextInt(MAPS.length);
+				mapCreates[i][j] = MAPS[random];
 				System.out.print(mapCreates[i][j]);
 			}
 			System.out.println();
